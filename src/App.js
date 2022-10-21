ac2b1bff7af90f0b19bb87a3520f7825f44017ba
serin-B <bangserin980302@gmail.com>
2022-10-21 19:16:33 +0900

    feat: create 버튼 생성

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { useState } from 'react';
+
+const Todowrapper = styled.div`
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Btn = styled.button`
+  width: 200px;
+  height: 50px;
+  color: white;
+  background-color: skyblue;
+  border: 1px solid skyblue;
+  border-radius: 25px;
+`;
 
 function App() {
+  const [todo, setTodo] = useState(0);
+
+  const handleClick = () => {
+    setTodo(todo + 1);
+  };
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Todo App</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Todowrapper>
+          <p>할일 {todo} 개 남았다!</p>
+          <Btn onClick={handleClick}>Create Todo</Btn>
+        </Todowrapper>
       </header>
     </div>
   );
